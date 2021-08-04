551404fb6662bc5532b27c743f95856044d5d62e
RiadOliveira <riad.oliveira@hotmail.com>
2021-08-04 15:47:42 -0300

    Main class clear

--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
-import java.time.LocalDate;
-
 class Main {
     public static void main(String args[]) {
-        LocalDate date = LocalDate.of(2021, 8, 17);
-
-        int startOfWeek = 17 - date.getDayOfWeek().getValue();
-
-        System.out.println(startOfWeek);
+        
     }
 }
