ea0159d5492e63d11944d06601f1789f55c8c78f
RiadOliveira <riad.oliveira@hotmail.com>
2021-10-05 17:16:22 -0300

    Metodos find do OrderProductBO

--- src/model/BO/OrderProductBO.java
+++ src/model/BO/OrderProductBO.java
@@ -1,5 +1,11 @@
 package model.BO;
 
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.util.UUID;
+
 import model.DAO.OrderDAO;
 import model.DAO.OrderProductDAO;
 import model.DAO.ProductDAO;
@@ -30,6 +36,121 @@ public class OrderProductBO {
         	System.out.println(err.getMessage());
 
             return false;
+        }
+    }
+
+    public static OrderProductVO findById(OrderProductVO orderProduct) {
+        try {
+            ResultSet findedOrderProductDB = orderProductDAO.findById(orderProduct);
+
+            OrderVO order = new OrderVO();
+            order.setId(UUID.fromString(findedOrderProductDB.getString("order_id")));
+            order = OrderBO.findById(order);
+            
+            ProductVO product = new ProductVO();
+            product.setId(UUID.fromString(findedOrderProductDB.getString("product_id")));
+            product = ProductBO.findById(product);
+            
+            OrderProductVO findedOrderProduct = new OrderProductVO();
+            findedOrderProduct.setId(UUID.fromString(findedOrderProductDB.getString("id")));
+            findedOrderProduct.setOrder(order);
+            findedOrderProduct.setProduct(product);
+            findedOrderProduct.setQuantity(findedOrderProductDB.getInt("quantity"));
+
+            return findedOrderProduct;
+        } catch (Exception err) {
+            //Handle exception.
+        	System.out.println(err.getMessage());
+            
+            return null;
+        }
+    }
+
+    public static List<OrderProductVO> findByOrder(OrderVO order) {
+        try {
+            ResultSet findedOrderProductsDB = orderProductDAO.findByOrder(order);
+            List<OrderProductVO> findedOrderProducts = new ArrayList<OrderProductVO>();
+
+            while(findedOrderProductsDB.next()) {
+                ProductVO product = new ProductVO();
+                product.setId(UUID.fromString(findedOrderProductsDB.getString("product_id")));
+                product = ProductBO.findById(product);
+                
+                OrderProductVO findedOrderProduct = new OrderProductVO();
+                findedOrderProduct.setId(UUID.fromString(findedOrderProductsDB.getString("id")));
+                findedOrderProduct.setOrder(order);
+                findedOrderProduct.setProduct(product);
+                findedOrderProduct.setQuantity(findedOrderProductsDB.getInt("quantity"));
+    
+                findedOrderProducts.add(findedOrderProduct);
+            }
+
+            return findedOrderProducts;
+        } catch (Exception err) {
+            //Handle exception.
+        	System.out.println(err.getMessage());
+            
+            return null;
+        }
+    }
+
+    public static List<OrderProductVO> findByProduct(ProductVO product) {
+        try {
+            ResultSet findedOrderProductsDB = orderProductDAO.findByProduct(product);
+            List<OrderProductVO> findedOrderProducts = new ArrayList<OrderProductVO>();
+
+            while(findedOrderProductsDB.next()) {
+                OrderVO order = new OrderVO();
+                order.setId(UUID.fromString(findedOrderProductsDB.getString("order_id")));
+                order = OrderBO.findById(order);
+                
+                OrderProductVO findedOrderProduct = new OrderProductVO();
+                findedOrderProduct.setId(UUID.fromString(findedOrderProductsDB.getString("id")));
+                findedOrderProduct.setOrder(order);
+                findedOrderProduct.setProduct(product);
+                findedOrderProduct.setQuantity(findedOrderProductsDB.getInt("quantity"));
+    
+                findedOrderProducts.add(findedOrderProduct);
+            }
+
+            return findedOrderProducts;
+        } catch (Exception err) {
+            //Handle exception.
+        	System.out.println(err.getMessage());
+            
+            return null;
+        }
+    }
+
+    public static List<OrderProductVO> findAll() {
+        try {
+            ResultSet findedOrderProductsDB = orderProductDAO.findAll();
+            List<OrderProductVO> findedOrderProducts = new ArrayList<OrderProductVO>();
+
+            while(findedOrderProductsDB.next()) {
+                OrderVO order = new OrderVO();
+                order.setId(UUID.fromString(findedOrderProductsDB.getString("order_id")));
+                order = OrderBO.findById(order);
+                
+                ProductVO product = new ProductVO();
+                product.setId(UUID.fromString(findedOrderProductsDB.getString("product_id")));
+                product = ProductBO.findById(product);
+                
+                OrderProductVO findedOrderProduct = new OrderProductVO();
+                findedOrderProduct.setId(UUID.fromString(findedOrderProductsDB.getString("id")));
+                findedOrderProduct.setOrder(order);
+                findedOrderProduct.setProduct(product);
+                findedOrderProduct.setQuantity(findedOrderProductsDB.getInt("quantity"));
+    
+                findedOrderProducts.add(findedOrderProduct);
+            }
+
+            return findedOrderProducts;
+        } catch (Exception err) {
+            //Handle exception.
+        	System.out.println(err.getMessage());
+            
+            return null;
         }
     }
 
