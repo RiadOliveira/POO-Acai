3a491dd8ca98ad469e8f0f1ceb19b47281a76860
RiadOliveira <riad.oliveira@hotmail.com>
2021-10-13 15:31:01 -0300

    View.java e LandingController

---
+++ src/controller/LandingController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import model.BO.UserBO;
+import model.VO.UserVO;
+
+public class LandingController {
+    @FXML private TextField cpfInput;
+    @FXML private Pane cpfInputContainer;
+
+    @FXML private TextField passwordInput;
+    @FXML private Pane passwordInputContainer;
+
+    public void signIn() {
+        UserVO user = new UserVO();
+
+        user.setCpf(cpfInput.getText());
+        user.setPassword(passwordInput.getText());
+
+        user = UserBO.signIn(user);
+
+        if(user == null) {
+            cpfInputContainer.setStyle(cpfInputContainer.getStyle() + "-fx-border-color: red;");
+            passwordInputContainer.setStyle(passwordInputContainer.getStyle() + "-fx-border-color: red;");
+        }
+    }
+}
