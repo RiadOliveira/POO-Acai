6a47aff169143e6b903bb241a95c78e9f794b4e7
Riad Oliveira de Morais <riad.oliveira@hotmail.com>
2021-11-09 21:16:53 -0300

    Dados da tabela de customers e employees

--- src/controller/screens/CustomersScreen.java
+++ src/controller/screens/CustomersScreen.java
@@ -1,11 +1,41 @@
 package controller.screens;
 
+import java.util.List;
+
 import controller.DashboardPageWithModal;
 import controller.DashboardPagesRedirect;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.BO.CustomerBO;
+import model.VO.CustomerVO;
 import utils.Modal;
 import view.ModalLoader;
 
 public class CustomersScreen extends DashboardPagesRedirect implements DashboardPageWithModal {
+    @FXML private TableView<CustomerVO> customersTable;
+
+    @FXML private TableColumn<CustomerVO, String> name;
+    @FXML private TableColumn<CustomerVO, String> cpf;
+    @FXML private TableColumn<CustomerVO, String> address;
+    @FXML private TableColumn<CustomerVO, String> phoneNumber;
+
+    public void initialize() {
+        ObservableList<CustomerVO> customers = FXCollections.observableArrayList();
+        List<CustomerVO> allCustomers = CustomerBO.findAll();
+
+        customers.addAll(allCustomers);
+        customersTable.setItems(customers);
+
+        name.setCellValueFactory(new PropertyValueFactory<CustomerVO, String>("name"));
+        cpf.setCellValueFactory(new PropertyValueFactory<CustomerVO, String>("cpf"));
+        address.setCellValueFactory(new PropertyValueFactory<CustomerVO, String>("address"));
+        phoneNumber.setCellValueFactory(new PropertyValueFactory<CustomerVO, String>("phoneNumber"));
+    }
+
     public void openModal() {
         ModalLoader.load(Modal.newCustomerModal);
     }
--- src/controller/screens/EmployeesScreen.java
+++ src/controller/screens/EmployeesScreen.java
@@ -1,11 +1,40 @@
 package controller.screens;
 
+import java.util.List;
+
 import controller.DashboardPageWithModal;
 import controller.DashboardPagesRedirect;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.BO.UserBO;
+import model.VO.UserVO;
 import utils.Modal;
 import view.ModalLoader;
 
 public class EmployeesScreen extends DashboardPagesRedirect implements DashboardPageWithModal {
+    @FXML private TableView<UserVO> employeesTable;
+
+    @FXML private TableColumn<UserVO, String> name;
+    @FXML private TableColumn<UserVO, String> cpf;
+    @FXML private TableColumn<UserVO, String> phoneNumber;
+
+    public void initialize() {
+        ObservableList<UserVO> employees = FXCollections.observableArrayList();
+        List<UserVO> allEmployees = UserBO.findAll();
+
+        employees.addAll(allEmployees);
+        employeesTable.setItems(employees);
+
+        name.setCellValueFactory(new PropertyValueFactory<UserVO, String>("name"));
+        cpf.setCellValueFactory(new PropertyValueFactory<UserVO, String>("cpf"));
+        phoneNumber.setCellValueFactory(new PropertyValueFactory<UserVO, String>("phoneNumber"));
+    }
+
+
     public void openModal() {
         ModalLoader.load(Modal.newEmployeeModal);
     }
