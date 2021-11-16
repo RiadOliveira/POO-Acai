ba51c8defaf17edd8515a2e4a2e7dc826fa3b2cd
valdirpn <valdiraires.pn@gmail.com>
2021-11-15 21:10:07 -0300

    sales fillTable

--- src/controller/screens/SalesScreen.java
+++ src/controller/screens/SalesScreen.java
@@ -1,7 +1,75 @@
 package controller.screens;
 
-import controller.DashboardPagesRedirect;
+import java.sql.SQLException;
+import java.util.List;
 
-public class SalesScreen extends DashboardPagesRedirect {
+import controller.DashboardPageWithTable;
+import controller.DashboardPagesRedirect;
+import errors.ValidationException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.BO.ProductBO;
+import model.VO.ProductVO;
+import model.VO.UserVO;
+import utils.Modal;
+import view.ModalLoader;
+
+public class SalesScreen extends DashboardPagesRedirect implements DashboardPageWithTable {
+	@FXML private TableView<ProductVO> productsTable;
+	
+	@FXML private TableColumn<ProductVO, String> name;
+	@FXML private TableColumn<ProductVO, Double> price;
+
+	@FXML private TableColumn<ProductVO, String> selectedProductName;
+	@FXML private TableColumn<ProductVO, Integer> selectedProductQuantity;
+	@FXML private TableColumn<ProductVO, Double> selectedProductType;
+	
+	private static UserVO selectedEmployee = null;
+	private List<ProductVO> allProducts = null;
+	
+	public void initialize() {
+		if(selectedEmployee != null) {
+            selectedEmployee = null;
+        }
+		
+		try {
+			fillTable();
+		} catch (SQLException | ValidationException err) {
+			System.out.println(err.getMessage());
+		}
+	}
+	
+	private void fillTable() throws SQLException, ValidationException {
+        ObservableList<ProductVO> products = FXCollections.observableArrayList();
+        allProducts = ProductBO.findAll();
+
+        products.addAll(allProducts);
+        productsTable.setItems(products);
+
+        name.setCellValueFactory(new PropertyValueFactory<ProductVO, String>("name"));
+        price.setCellValueFactory(new PropertyValueFactory<ProductVO, Double>("price"));
+    }
+	
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void openModal() {
+		// TODO Auto-generated method stub
+		ModalLoader.load(Modal.finishSaleModal);
+	}
     
 }
